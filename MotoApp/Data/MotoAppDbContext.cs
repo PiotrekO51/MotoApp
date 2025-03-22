5aaf48f29e560b1cdf2fc0a271b5a6e8e0db5cc2
PiotrekO51 <obojowy@wp.pl>
2025-03-22 15:50:54 +0100

    Zmiana 22-03-2025

---
+++ MotoApp/Data/MotoAppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MotoApp.Entities;
+
+namespace MotoApp.Data;
+
+public class MotoAppDbContext : DbContext
+{
+    public MotoAppDbContext(DbContextOptions<MotoAppDbContext> options)
+       : base(options)
+    {
+    }
+
+    public DbSet<Employee> Employees => Set<Employee>();
+    public DbSet<Car> Cars => Set<Car>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseInMemoryDatabase("StorageMotoApp3");
+    }
+
+}
